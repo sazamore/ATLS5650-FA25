835a26a6760efcad0b930b54278de6faf35a8c80
Dr Z <1869730+sazamore@users.noreply.github.com>
2025-10-28 21:22:29 -0600

    use of a tuple to keep the madlib safe!

---
+++ tuple_MadLib.py
@@ -0,0 +1,21 @@
+
+print("Mad Libs: Enter 8 adjectives (easy version)")
+
+a1 = input("Adjective 1: ")
+a2 = input("Adjective 2: ")
+a3 = input("Adjective 3: ")
+a4 = input("Adjective 4: ")
+a5 = input("Adjective 5: ")
+a6 = input("Adjective 6: ")
+a7 = input("Adjective 7: ")
+a8 = input("Adjective 8: ")
+
+poem = (
+    "As " + a1 + " as a wolf, as " + a2 + " as a tooth\n"
+    "As " + a3 + " as a bite, as " + a4 + " as the night\n"
+    "As " + a5 + " as a song, as " + a6 + " as a wrong\n"
+    "As " + a7 + " as a road, as " + a8 + " as a toad"
+)
+
+print("\nYour Mad Lib:\n")
+print(poem)

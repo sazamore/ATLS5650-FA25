0ac0a16abee8ca3093c5be3b3b179d67b7c9ba3a
Dr Z <1869730+sazamore@users.noreply.github.com>
2025-10-28 21:10:23 -0600

    This madlib uses engaging prompts

---
+++ Engagement_Madlib.py
@@ -0,0 +1,25 @@
+
+print( "Let's help R. Frost write about life choices!")
+
+road = input("what's your favorite city? ")
+action = input("I'm coming with. Do we fly? walk? teleport?.. ")
+surface = input("Was that trip smooth or rough?  ")
+adjective = input("what's the opposite? ")
+noun = input("how much time has passed? ")
+verb = input("how much time has that felt like? ")
+
+
+print("\nStunning! I'm sure he'd agree you are a poet too!\n")
+
+
+print(f"""Two roads diverged in {road},
+And sorry I could not {action} both
+And be one traveler, long I stood
+And looked down one as far as I could
+To where it bent in the underbrush;
+
+Then took the other, as just as fair,
+And having perhaps the better {surface},
+Because it was grassy and wanted to be{adjective};
+Though as for that the passing {noun}
+Had {verb} to them really about the same...""")
